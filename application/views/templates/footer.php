c507b6a82c98ed39df24d4b37dab29046e230f17
nikidc <nikidjanuar44@gmail.com>
2022-04-16 14:49:36 +0700

    test2

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -239,4 +239,3 @@
 <script src="<?php echo base_url() ?>assets/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="<?php echo base_url() ?>assets/dist/js/demo.js"></script>
-<!-- test --> 
